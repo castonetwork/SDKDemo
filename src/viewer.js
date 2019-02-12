cfdf43172a3f8538f78c28cfdab0ebeb4b9dbda8
jhlee <jhlee@casto.network>
2019-02-12 14:58:16 +0900

    add: add a viewer example

---
+++ src/viewer.js
@@ -0,0 +1,25 @@
+import "@babel/polyfill";
+import Casto from "@casto/sdk";
+
+const initApp = async () => {
+  window.casto = new Casto({
+    type: "viewer"
+  });
+
+  Object.assign(casto, {
+    onNodeIntiated: e => console.log("[event] node init"),
+    onReadyToCast: peerId=> console.log("[event] ready to cast", peerId),
+    onClosed: ()=> document.getElementById("media").srcObject = null
+  });
+
+  const startButtonElement = document.getElementById("start");
+  for (;;) {
+    await new Promise((resolve)=> {
+      
+    });
+    document.getElementById("media").srcObject = await casto.start();
+    startButtonElement.textContent = "stop";
+  }
+};
+
+document.addEventListener("DOMContentLoaded", initApp);
