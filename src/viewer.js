64118ef5002e674178b372134f8af3f7225a85bf
jhlee <jhlee@casto.network>
2019-02-20 17:26:14 +0900

    add: add a channel list

--- src/viewer.js
+++ src/viewer.js
@@ -1,24 +1,43 @@
 import "@babel/polyfill";
 import Casto from "@casto/sdk";
+const media = document.getElementById('media');
 
 const initApp = async () => {
   window.casto = new Casto({
     type: "viewer"
   });
 
-  let channels = {};
-
+  const channelsElement = document.getElementById('channels');
+  const getChannel = async peerId => {
+    console.log("getChannel", peerId);
+    media.srcObject = await Casto.getChannel(peerId);
+  };
+  const updateChannel = ({peerId, info})=> {
+    let peerElement = document.getElementById(peerId);
+    if (!peerElement) {
+      peerElement = document.createElement('li');
+      peerElement.setAttribute('id', peerId);
+      channelsElement.appendChild(peerElement);
+      channelsElement.addEventListener('click', ()=>getChannel(peerId), false);
+    }
+    peerElement.textContent = `${info.title}: ${peerId}`;
+  };
+  const removeChannel = peerId => {
+    document.getElementById(peerId).remove();
+  };
   Object.assign(casto, {
     onNodeInitated: e => console.log("[event] node init"),
     onReadyToCast: peerId=> console.log("[event] ready to cast", peerId),
     onClosed: ()=> document.getElementById("media").srcObject = null,
-    onSendChannelsList: e => console.log("channels list", e),
-    onSendChannelAdded: ({peerId, info}) => console.log("added channel", peerId, info),
-    onSendChannelRemoved: e => console.log("disconnected prism", e)
+    onSendChannelsList: channels => {
+      for (const channel in channels) {
+        updateChannel({ peerId: channel, info: channels[channel]});
+      }
+    },
+    onSendChannelAdded: updateChannel,
+    onSendChannelRemoved: removeChannel,
   });
 
-  new Promise(r=>casto.onSendChannelsList)
-  // document.getElementById("media").srcObject = await casto.start();
 };
 
 document.addEventListener("DOMContentLoaded", initApp);
